f3d0777c48cebfebcbf5cea9f37d6209dc1959f8
hinniss <126616486+hinniss@users.noreply.github.com>
2023-07-18 20:54:42 -0400

    Add files via upload

---
+++ Eight Ball/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import EightBall from './EightBall';
+
+function App() {
+  return (
+    <div className='Eightball'>
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
